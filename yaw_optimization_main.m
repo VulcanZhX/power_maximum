ebc827533cd45d39b8c2e51dbae7a2cd356c5859
Director Krennic <zhaoqc_spx@sjtu.edu.cn>
2025-08-04 16:22:03 +0800

    final1

--- yaw_optimization_main.m
+++ yaw_optimization_main.m
@@ -30,16 +30,16 @@ end
 
 % 打印输入：风向，风速，青州12与青州3的AGC功率百分比
 fprintf('本次执行结果为：风向: %.0f, 风速: %.1f, 青州12风场AGC: %.2f%%, 青州3风场AGC: %.2f%%\n', ...
-    wind_direction(wd), vel_sets(vel), 100*qingzhou12_agc_perc(1), 100*qingzhou3_agc_perc(1));
-    
+    wind_direction(wd), vel_sets(vel), 100*qingzhou12_agc_perc(wd), 100*qingzhou3_agc_perc(vel));
+
 %% 计时开始
 tic
 swi.yaw_optimization_tracking_life_ipopt(qingzhou12_agc(1), qingzhou3_agc(1));
 time = toc; % 计时结束并记录
-objectives((wd - 1) * length(vel_sets) + vel, 1) = swi.get_farm_power();
+objectives = swi.get_farm_power();
 % 打印运行时间
 fprintf('本次执行周期为: %.2fs\n', time);
 % 判定是否小于29s
-if times((wd - 1) * length(vel_sets) + vel, 1) <= 29
+if time <= 29
     fprintf('本次测试周期时间小于29秒\n');
 end
